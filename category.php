0737102b08df066372005ec143cf1b87471e6de0
krishma <np03cs4s240036@heraldcollege.edu.np>
2025-05-12 00:53:55 +0545

    upadated better ui

--- category.php
+++ category.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 if (!isset($_SESSION['email']) || $_SESSION['email'] == false) {
-    header('Location: category.php');
+    header('Location: role-login.php');
     exit();
 }
 
